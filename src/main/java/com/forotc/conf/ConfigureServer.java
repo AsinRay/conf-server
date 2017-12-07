f5229f651dd7191d0c4b6f0a11e36ec747348bcc
idoz <dozray@163.com>
2017-12-07 16:06:44 +0800

    init project

---
+++ src/main/java/com/forotc/conf/ConfigureServer.java
@@ -0,0 +1,13 @@
+package com.forotc.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigureServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigureServer.class, args);
+    }
+}
