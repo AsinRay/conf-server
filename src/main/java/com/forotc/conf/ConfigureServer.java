7061ada566181325706ba01b00bf1d97fc2b780a
idoz <dozray@163.com>
2017-12-08 14:44:56 +0800

    init project

--- src/main/java/com/forotc/conf/ConfigureServer.java
+++ src/main/java/com/forotc/conf/ConfigureServer.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+/**
+ * iLook conf server.
+ * @author Asin Liu
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigureServer {
