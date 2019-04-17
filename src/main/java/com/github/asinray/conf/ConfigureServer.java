7d2ad8052181ad2d6242b7c949b3339d6f44d836
asin <codezone@163.com>
2019-04-17 18:34:44 +0800

    change mvn to gradle

--- src/main/java/com/github/asinray/conf/ConfigureServer.java
+++ src/main/java/com/github/asinray/conf/ConfigureServer.java
@@ -1,4 +1,4 @@
-package com.forotc.conf;
+package com.github.asinray.conf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
