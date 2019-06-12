c1d1dba94b2f342e85a26892fce4ca50553822b8
asin <codezone@163.com>
2019-06-12 18:37:36 +0800

    JVM parameter

--- src/main/resources/importCA.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-KS=$JAVA_HOME/lib/security/cacerts
-keytool -list -v -keystore $KS -storepass changeit | grep cnfsrv-ca
-keytool -export -v -alias cnfsrv -keystore cnfsrv.p12 -storepass srv666 -storetype PKCS12 -rfc -file ca.cer
-keytool -import -v -noprompt -trustcacerts -alias cnfsrv-ca -file ca.cer -keystore $KS
-keytool -list -v -keystore $KS -storepass changeit | grep cnfsrv-ca
